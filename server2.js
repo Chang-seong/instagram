d2e16f9bbedb99813f5353bd36d1105fc6cac1e1
Chang-seong <tgb0118@naver.com>
2025-06-09 10:47:45 +0900

    server2.js

--- server2.js
+++ server2.js
@@ -4,14 +4,14 @@ const uri = "mongodb+srv://plzz:test1234@cluster0.65etpdx.mongodb.net/myboard?re
 
 MongoClient.connect(uri)
   .then(client => {
-    console.log("✅ 연결 성공!");
+    console.log("연결 성공!");
     return client.db("myboard").collection("test").insertOne({ ok: true });
   })
   .then(result => {
-    console.log("✅ 데이터 삽입 성공:", result.insertedId);
+    console.log("데이터 삽입 성공:", result.insertedId);
     process.exit();
   })
   .catch(err => {
-    console.error("❌ 최종 에러:", err);
+    console.error("최종 에러:", err);
     process.exit();
   });
